81dceb4c121c7c2373ea60f7704dd495ef1ce947
lwj <jg15575446613@163.com>
2024-05-12 00:11:38 +0800

    learn template

---
+++ 04/001section-4-1-STARTER/app.js
@@ -0,0 +1,14 @@
+let vm = Vue.createApp({
+  
+})
+
+vm.component('hello', {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  },
+  template: `<h1>{{message}}</h1>`
+})
+
+vm.mount('#app')

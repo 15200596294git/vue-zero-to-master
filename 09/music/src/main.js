cdee6ba2d2f239ed21ee46da6e32157ebe67a689
lwj <jg15575446613@163.com>
2024-05-12 23:58:20 +0800

    challenge

---
+++ 09/music/src/main.js
@@ -0,0 +1,18 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/base.css'
+import './assets/main.css'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
